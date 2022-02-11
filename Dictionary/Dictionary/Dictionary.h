d2d27009ca2e7040e3921857af35528982b1150b
Miles <miles.gr.lee@gmail.com>
2022-02-11 10:02:36 -0600

    Start Commit

---
+++ Dictionary/Dictionary/Dictionary.h
@@ -0,0 +1,17 @@
+#pragma once
+
+template<typename T, typename TKey, typename TValue>
+struct Item 
+{
+	Tkey;
+	TValue;
+};
+
+template<typename T, typename TKey, typename TValue>
+class Dictionary 
+{
+	template<typename TKey, typename TValue>
+	Dictionary();
+	template<typename TKey, typename TValue>
+	Dictionary(const Dictionary & other);
+};
